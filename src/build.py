5c0e96b54834cb107366ad0b181889727f805823
Max-42 <max@oppermann.fun>
2021-05-05 14:02:43 +0200

    updated build script

--- src/build.py
+++ src/build.py
@@ -2,22 +2,18 @@ import markdown
 import os
 import shutil
 
-articles=[]
 
 filepath = os.path.dirname(os.path.realpath('__file__'))
-
-
 
 
 def prepare():
     filepath = os.path.dirname(os.path.realpath('__file__'))
     filepath = os.path.join(filepath, 'src' , 'public')
-    print(filepath)
+    print("Running in: " + filepath)
     if os.path.exists(filepath):
         shutil.rmtree(filepath)
     else:
         print("No files to cleanup.")
-
 
     try:
         os.mkdir(filepath)
@@ -26,24 +22,51 @@ def prepare():
     else:
         print ("Successfully created the directory %s " % filepath)
     
+def scanfiles():
+    filepath = os.path.dirname(os.path.realpath('__file__'))
+    filepath = os.path.join(filepath, 'src' , 'articles')
+    files = []
+    for (dirpath, dirnames, filenames) in os.walk(filepath):
+        files.extend(filenames)
+        break
+    return files
 
 
 prepare()
 
+articles = scanfiles()
+print("Files to phrase: " + str(articles))
 
 
-filepathbefore = os.path.join(filepath, 'src' , 'articles', 'test.md')
 
-filepathafter= os.path.join(filepath, 'src' , 'public', 'test.html')
+def phrase(articles):
+    filepath = os.path.dirname(os.path.realpath('__file__'))
 
-print(filepath)
+    for article in articles:
+        print("Phrasing " + article)
+        article = os.path.splitext(article)[0]
 
-with open (filepathbefore, 'r' ) as f:
-    text = f.read()
-    f.close
-    html = markdown.markdown(text)
-    f.close
+        print(article)
 
-with open (filepathafter, 'w') as f:
-    f.write(html)
-    f.close()
+
+
+        filenamebefore = article + '.md'
+        filepathbefore = os.path.join(filepath, 'src' , 'articles', filenamebefore)
+        
+        filenameafter = article + '.html'
+        filepathafter= os.path.join(filepath, 'src' , 'public', filenameafter)
+
+
+        print("Using filepath: " + filepath)
+
+        with open (filepathbefore, 'r' ) as f:
+            text = f.read()
+            f.close
+            html = markdown.markdown(text)
+            f.close
+
+        with open (filepathafter, 'w') as f:
+            f.write(html)
+            f.close()
+
+phrase(articles)
